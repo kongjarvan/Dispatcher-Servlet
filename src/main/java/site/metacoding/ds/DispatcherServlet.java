b151a43560410cc9d1e85c78fa261a574b0d60e5
kongjarvan <wjsdudwox@gmail.com>
2022-08-30 11:55:19 +0900

    디스패쳐 서블릿 직접 만들어보기

---
+++ src/main/java/site/metacoding/ds/DispatcherServlet.java
@@ -0,0 +1,53 @@
+package site.metacoding.ds;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doProcess(req, resp);
+	}
+
+	private void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doProcess 요청됨");
+		String httpMethod=req.getMethod();
+		System.out.println(httpMethod); // 어떤 메서드를 요청 받았는지 출력해줌
+		String identifier = req.getRequestURI();
+		System.out.println(identifier); // 로컬호스트 기본주소 (localhost:8000) 이후의 주소를 출력해줌
+		
+		UserController c = new UserController();
+		if(identifier.equals("/join")){
+			c.join();
+		}else if(identifier.equals("/login")) {
+			c.login();
+		}else {
+			System.out.println("잘못된 요청입니다.");
+		}
+	}
+	
+
+	
+}
